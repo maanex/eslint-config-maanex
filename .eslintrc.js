6ab31fe671d0083fa039adca8573be41a8b0c598
maanex <maan91315@gmail.com>
2021-01-20 08:36:37 +0100

    remove dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,65 @@
+
 module.exports = {
-  root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    'jest/globals': true
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
   ],
-  plugins: [
-  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  settings: {
+    'import/resolver': {
+      node: { extensions: ['.js', '.mjs'] }
+    }
+  },
   rules: {
+    // FROM NUXTJS
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unresolved': 'off',
+    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'prefer-const': ['error', {
+      destructuring: 'any',
+      ignoreReadBeforeAssign: false
+    }],
+    'no-lonely-if': 'error',
+    curly: ['error', 'all'],
+    'require-await': 'error',
+    'dot-notation': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'no-useless-rename': 'error',
+    'unicorn/error-message': 'error',
+    'unicorn/escape-case': 'error',
+    'unicorn/no-array-instanceof': 'error',
+    'unicorn/no-new-buffer': 'error',
+    'unicorn/no-unsafe-regex': 'off',
+    'unicorn/number-literal-case': 'error',
+    'unicorn/prefer-exponentiation-operator': 'error',
+    'unicorn/prefer-includes': 'error',
+    'unicorn/prefer-starts-ends-with': 'error',
+    'unicorn/prefer-text-content': 'error',
+    'unicorn/prefer-type-error': 'error',
+    'unicorn/throw-new-error': 'error',
+    'vue/no-parsing-error': ['error', {
+      'x-invalid-end-tag': false
+    }],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 5
+    }],
+    
+    // FROM NUXTJS TS
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
+    'no-unused-vars': 'off',
+
+    // CUSTOM
     'no-multiple-empty-lines': [ 'warn', { max: 2, maxEOF: 1 } ],
     'padded-blocks': [ 'error', { classes: 'always' } ],
     'array-bracket-spacing': [ 'error', 'always', { arraysInArrays: false } ],
